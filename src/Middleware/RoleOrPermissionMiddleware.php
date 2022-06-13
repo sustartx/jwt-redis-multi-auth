2856fbd716ff5c9e68ec23864d9fdded5768029d
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2022-06-13 16:10:14 +0300

    Gereksiz notlar temizlendi ve setAuthedUser işleminin sonucu TokenInvalidException ile kontrol edildi.

--- src/Middleware/RoleOrPermissionMiddleware.php
+++ src/Middleware/RoleOrPermissionMiddleware.php
@@ -12,26 +12,23 @@ class RoleOrPermissionMiddleware extends BaseMiddleware
 {
     public function handle($request, Closure $next, $roleOrPermission)
     {
-//        \DB::enableQueryLog();
-
         try {
             $this->setIfClaimIsNotExist($request);
         } catch (TokenExpiredException | TokenInvalidException | JWTException $e) {
             return $this->getErrorResponse($e, Response::HTTP_UNAUTHORIZED);
         }
 
-        $this->setAuthedUser($request);
+        try {
+            $this->setAuthedUser($request);
+        } catch (TokenInvalidException $e) {
+            return $this->getErrorResponse($e, Response::HTTP_UNAUTHORIZED);
+        }
 
         $rolesOrPermissions = is_array($roleOrPermission) ? $roleOrPermission : explode('|', $roleOrPermission);
 
         if (!$request->authedUser->hasAnyRole($rolesOrPermissions) && !$request->authedUser->hasAnyPermission($rolesOrPermissions)) {
             return $this->getErrorResponse('RoleOrPermissionException', Response::HTTP_FORBIDDEN);
         }
-
-//        $query = \DB::getQueryLog();
-//        \DB::disableQueryLog();
-
-//        dd($query);
 
         return $next($request);
     }
